c2bfe216dc151b601a58b2b768ef5e5b29716d15
N4huelMO <nahuelmesa2014@hotmail.com>
2024-01-04 13:49:19 -0300

    Header component and hero section created

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { PiShoppingCartSimpleBold } from "react-icons/pi";
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+  /* pathname to highlight the active tab  
+  const pathname = usePathname(); */
+
+  return (
+    <header className="text-white bg-black/80 fixed top-0 z-50 w-full py-6">
+      <div className="flex justify-around items-center">
+        <Link href="/">
+          <Image priority src="/logo.png" alt="a" width={300} height={300} />
+        </Link>
+
+        <nav className="flex gap-10 items-center text-lg uppercase">
+          <Link href="#" className="hover:text-[#b36f34]">
+            Home
+          </Link>
+          <Link href="#" className="hover:text-[#b36f34]">
+            Menu
+          </Link>
+          <Link href="#" className="hover:text-[#b36f34]">
+            Blog
+          </Link>
+          <Link href="#" className="hover:text-[#b36f34]">
+            About us
+          </Link>
+
+          <div className="hover:text-[#b36f34] cursor-pointer">
+            <PiShoppingCartSimpleBold size={20} />
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Roboto_Condensed } from "next/font/google";
+
+const robotoCondensed = Roboto_Condensed({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${robotoCondensed.className} bg-neutral-800 text-white`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
