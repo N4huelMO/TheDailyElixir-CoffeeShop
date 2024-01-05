127612618c1067df7993455c345b119720e39d22
N4huelMO <nahuelmesa2014@hotmail.com>
2024-01-05 16:27:46 -0300

    Responsive design of header component and hero section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,82 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { PiShoppingCartSimpleBold } from "react-icons/pi";
 import { usePathname } from "next/navigation";
 
+const navigationLinks = [
+  { name: "Home", href: "/" },
+  { name: "Menu", href: "#" },
+  { name: "Blog", href: "#" },
+  { name: "About us", href: "#" },
+];
+
 const Header = () => {
-  /* pathname to highlight the active tab  
-  const pathname = usePathname(); */
+  const pathname = usePathname();
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <header id="header-nav" className="text-white fixed top-0 z-50 w-full py-6">
-      <div className="flex justify-around items-center">
+    <header
+      id="header-nav"
+      className="text-white bg-black/35 md:bg-transparent fixed top-0 z-50 w-full py-6"
+    >
+      <div className="flex justify-between px-8 md:px-0 md:justify-around items-center">
         <Link href="/">
-          <Image priority src="/logo.png" alt="a" width={350} height={350} />
+          <Image
+            priority
+            src="/logo.png"
+            alt="logo"
+            width={350}
+            height={350}
+            className="hidden md:block"
+          />
+
+          <Image
+            priority
+            src="/logo.png"
+            alt="logo"
+            width={200}
+            height={200}
+            className="block md:hidden"
+          />
         </Link>
 
-        <nav className="flex gap-10 items-center text-lg uppercase">
-          <Link href="#" className="hover:text-[#b36f34]">
-            Home
-          </Link>
-          <Link href="#" className="hover:text-[#b36f34]">
-            Menu
-          </Link>
-          <Link href="#" className="hover:text-[#b36f34]">
-            Blog
-          </Link>
-          <Link href="#" className="hover:text-[#b36f34]">
-            About us
-          </Link>
+        <button
+          className="flex flex-col justify-center items-center cursor-pointer md:hidden"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          <div
+            className={`bg-[#b36f34] w-6  h-0.5 transition-all ${
+              isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+            }`}
+          />
+          <div
+            className={`bg-[#b36f34] w-6  h-0.5 transition-all my-0.5 ${
+              isOpen ? "opacity-0" : "opacity-100"
+            }`}
+          />
+          <div
+            className={`bg-[#b36f34] w-6  h-0.5 transition-all ${
+              isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+            }`}
+          />
+        </button>
+
+        <nav className="hidden gap-5 xl:gap-10 items-center text-lg uppercase md:flex">
+          {navigationLinks.map((link, i) => (
+            <Link
+              key={i}
+              href={link.href}
+              className={`hover:text-[#b36f34] font-bold ${
+                pathname === link.href && "text-[#b36f34]"
+              }`}
+            >
+              {link.name}
+            </Link>
+          ))}
 
           <div className="hover:text-[#b36f34] cursor-pointer">
             <PiShoppingCartSimpleBold size={20} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,14 +27,14 @@ export default function Home() {
       <Header />
 
       <div
-        className="h-screen flex flex-col bg-cover bg-no-repeat bg-center text-white px-28 pb-20"
+        className="h-screen flex flex-col bg-cover bg-no-repeat bg-center text-white px-5 md:px-28 pb-20"
         style={{
           backgroundImage: `linear-gradient(0deg, rgba(1,0,0,0.8) 0%, rgba(0,0,0,0.6) 50%, rgba(0,0,0,0.5) 100%),url("/hero-image.jpg")`,
         }}
       >
-        <div className="flex flex-col items-end justify-center drop-shadow-xl">
-          <div className="mt-48 w-max">
-            <h1 className="text-6xl font-semibold">
+        <div className="flex flex-col md:items-end justify-center text-center md:text-left drop-shadow-xl">
+          <div className="mt-48 md:w-max bg-black/30 px-4 py-6 rounded-md md:px-0 md:py-0 md:bg-transparent -z-20">
+            <h1 className="text-5xl lg:text-6xl font-semibold">
               Start your day with an{" "}
               <span
                 className={`text-[#b36f34] underline underline-offset-8 uppercase ${spectralSc.className}`}
@@ -43,7 +43,7 @@ export default function Home() {
               </span>
             </h1>
 
-            <h2 className="text-4xl mt-5 font-semibold">
+            <h2 className="text-3xl md:text-4xl mt-5 font-semibold">
               Start your day enjoying a{" "}
               <span
                 className={`text-[#b36f34] underline underline-offset-4 uppercase ${spectralSc.className}`}
@@ -52,7 +52,7 @@ export default function Home() {
               </span>
             </h2>
 
-            <div className="mt-10 flex gap-10 text-lg">
+            <div className="mt-10 flex gap-10 text-lg justify-center md:justify-normal">
               <button className="capitalize bg-[#b36f34] hover:bg-[#614126] px-5 py-2 duration-[300ms]">
                 order now
               </button>
@@ -68,7 +68,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex-grow flex items-end gap-10">
+        <div className="flex-grow flex items-end justify-center md:justify-start gap-10">
           {socials.map((social, i) => (
             <Link
               key={i}
